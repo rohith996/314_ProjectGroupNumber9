6df16edc69bf16d528d45736d17aa60459db248e
harshithreddypeddolla <harshithreddypeddolla123@gmail.com>
2020-02-08 15:45:59 -0500

    Login Activity Created

--- 314_Debugging/app/src/main/java/com/example/a314_debugging/LoginActivity.java
+++ 314_Debugging/app/src/main/java/com/example/a314_debugging/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.a314_debugging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText uname,upass;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        uname=findViewById(R.id.editText4);
+        upass=findViewById(R.id.editText5);
+        login=findViewById(R.id.button2);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
