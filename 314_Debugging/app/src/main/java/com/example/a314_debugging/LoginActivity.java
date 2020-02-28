d5eba22927529b8576b9c81c4fb27b86cf6c5996
Prasad2083 <varavam2083.com>
2020-02-28 00:28:28 -0500

    Login Activity created using Firebase

--- 314_Debugging/app/src/main/java/com/example/a314_debugging/LoginActivity.java
+++ 314_Debugging/app/src/main/java/com/example/a314_debugging/LoginActivity.java
@@ -1,33 +1,66 @@
 package com.example.a314_debugging;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText uname,upass;
-    Button login,signup;
+    EditText unemail,upass;
+    Button login;
+    TextView register;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        uname=findViewById(R.id.editText4);
+        unemail=findViewById(R.id.editText4);
         upass=findViewById(R.id.editText5);
         login=findViewById(R.id.button2);
-        signup=findViewById(R.id.button3);
+        register=findViewById(R.id.signup);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(LoginActivity.this,Home.class);
-                startActivity(i);
+                firebaseAuth=FirebaseAuth.getInstance();
+                String mail=unemail.getText().toString();
+                String pwd=upass.getText().toString();
+                if (TextUtils.isEmpty(mail)){
+                    unemail.setError("Enter Valid Email");
+                }
+                if (TextUtils.isEmpty(pwd)){
+                    upass.setError("Enter valid password");
+                }
+                firebaseAuth.signInWithEmailAndPassword(mail,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(LoginActivity.this,Home.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
             }
         });
-        signup.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(LoginActivity.this,MainActivity.class);
--- 314_Debugging/app/src/main/java/com/example/a314_debugging/MainActivity.java
+++ 314_Debugging/app/src/main/java/com/example/a314_debugging/MainActivity.java
@@ -1,33 +1,77 @@
 package com.example.a314_debugging;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-private EditText username,password,confirmpassword;
+private EditText usermail,password,confirmpassword;
 private Button signup;
 private TextView signin;
+FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        username = findViewById(R.id.editText);
+        usermail = findViewById(R.id.editText);
         password = findViewById(R.id.editText2);
         confirmpassword = findViewById(R.id.editText3);
         signup = findViewById(R.id.button);
         signin = findViewById(R.id.textView);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth=FirebaseAuth.getInstance();
+                String mail=usermail.getText().toString();
+                String pwd=password.getText().toString();
+                String cpwd=confirmpassword.getText().toString();
+                if (TextUtils.isEmpty(mail)){
+                    usermail.setError("Enter Email");
+                }
+                if (TextUtils.isEmpty(pwd)){
+                    password.setError("Enter Valid Password");
+                }
+                if (pwd.length()>=10){
+                    password.setError("Enter Password less than 10 characters");
+                }
+                if (TextUtils.isEmpty(cpwd)){
+                    confirmpassword.setError("Confrim Password should not be empty");
+                }
+                firebaseAuth.createUserWithEmailAndPassword(mail,pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"User Created",Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this,"Registration Failed",Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+            }
+        });
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(i);
+            public void onClick(View view) {
+                Intent login=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(login);
             }
         });
     }
