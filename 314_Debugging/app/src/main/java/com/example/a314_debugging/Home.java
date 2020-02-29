761acabbc57f5b29414c54f82f6dcfd1bd195c2c
harshithreddypeddolla <harshithreddypeddolla123.com>
2020-02-28 23:08:54 -0500

    created POJO class for data fileds required

--- 314_Debugging/app/src/main/java/com/example/a314_debugging/Home.java
+++ 314_Debugging/app/src/main/java/com/example/a314_debugging/Home.java
@@ -11,5 +11,7 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+
     }
 }
---
+++ 314_Debugging/app/src/main/java/com/example/a314_debugging/Reservations.java
@@ -0,0 +1,40 @@
+package com.example.cars_for_us;
+
+public class Reservations{
+    int image;
+    String indate,outdate,price;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    Reservations(int image, String indate, String outdate, String price) {
+        this.image=image;
+        this.indate=indate;
+        this.outdate=outdate;
+        this.price=price;
+
+    }
+    public String getIndate() {
+        return indate;
+    }
+    public void setIndate(String indate) {
+        this.indate = indate;
+    }
+    public String getOutdate() {
+        return outdate;
+    }
+    public void setOutdate(String outdate) {
+        this.outdate = outdate;
+    }
+    public int getImage() {
+        return image;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
